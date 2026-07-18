9085d92f05fc86660854a39b1581c1dd49893fd3
abhiagarwal030906-lang <abhiagarwal030906@gmail.com>
2026-07-18 21:16:49 +0530

    Time: 1 ms (26.07%), Space: 8.7 MB (10.81%) - LeetHub

---
+++ 2652-sum-multiples/2652-sum-multiples.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int sumOfMultiples(int n) {
+        int count=0;
+        for(int i=1;i<=n;i++){
+            if(i%3==0){
+                count+=i;
+            }
+            else if(i%5==0){
+                count+=i;
+            }
+            else if(i%7==0){
+                count+=i;
+            }
+        }
+        return count;
+    }
+};
