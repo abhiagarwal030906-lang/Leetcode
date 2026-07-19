f61d94879d206f4045bb91204ccb319f969d61c3
abhiagarwal030906-lang <abhiagarwal030906@gmail.com>
2026-07-19 19:54:58 +0530

    Time: 0 ms (100%), Space: 9.1 MB (55.32%) - LeetHub

--- 3019-number-of-changing-keys/3019-number-of-changing-keys.cpp
+++ 3019-number-of-changing-keys/3019-number-of-changing-keys.cpp
@@ -2,12 +2,12 @@ class Solution {
 public:
     int countKeyChanges(string s) {
       int count=0;
-      for(int i=0;i<s.length();i++){
+      for(int i=0;i<s.length()-1;i++){
         if((int)s[i]-(int)s[i+1]!=32 && (int)s[i]-(int)s[i+1]!=-32 && (int)s[i]-(int)s[i+1]!=0){
             count++;
         }
        
       }
-      return count-1;
+      return count;
     }
 };
